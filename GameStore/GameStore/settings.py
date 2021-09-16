939ef0f81453a2f00af27f86a3bc733e7177f6b2
fsmardani <42679276+fsmardani@users.noreply.github.com>
2021-09-16 22:12:35 +0430

    commit

--- GameStore/GameStore/settings.py
+++ GameStore/GameStore/settings.py
@@ -88,7 +88,9 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
+AUTHENTICATION_BACKENDS = (
+        'django.contrib.auth.backends.ModelBackend',
+)
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- GameStore/users/admin.py
+++ GameStore/users/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import User,normalUserProfile,vipUserProfile
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
+
 admin.site.register(User)
 admin.site.register(normalUserProfile)
 admin.site.register(vipUserProfile)
